f6979e7c81c4207da66d9b3342e8cad4a18e2a98
Koichi Shiraishi <zchee.io@gmail.com>
2021-11-05 10:54:10 +0900

    config/boilerplate/go: fix Copyright header
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- .config/boilerplate/go/empty.s
+++ .config/boilerplate/go/empty.s
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: YEAR The PKG Authors
+// Copyright YEAR The AUTHOR Authors
 // SPDX-License-Identifier: BSD-3-Clause
 
 // The presence of this file allows the package to use the
